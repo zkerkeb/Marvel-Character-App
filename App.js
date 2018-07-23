1b9b5c6dbf51dd151438d0945a5aed6bdbd6f21f
Kerkeb Zakaria <hikkary@MacBook-Air-de-Kerkeb.local>
2018-07-23 18:12:16 +0200

    refacto(app Index): minor chnages

--- App.js
+++ App.js
@@ -8,12 +8,16 @@ import { theme } from './app/config/theme'
 
 import { PersistGate } from 'redux-persist/lib/integration/react'
 
+import { Text } from 'react-native'
+
 export default class App extends React.Component {
   render() {
+    // Do a function to purge every 24 or 48h
+    // persistor.purge()
     return (
       <ThemeProvider theme={theme}>
         <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
+          <PersistGate loading={<Text>Loading</Text>} persistor={persistor}>
             <StackNavigator />
           </PersistGate>
         </Provider>
